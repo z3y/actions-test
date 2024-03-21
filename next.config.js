851a066aa2b62b40b6987ea447f4a8a7b15aa76a
z3y <z3y@protonmail.com>
2024-03-21 22:54:15 +0100

    add index page

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+    
+})
+
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+      },
+  }
+
+  module.exports = withNextra(nextConfig)
+   
+  // If you have other Next.js configurations, you can pass them as the parameter:
+  // module.exports = withNextra({ /* other next.js config */ })
